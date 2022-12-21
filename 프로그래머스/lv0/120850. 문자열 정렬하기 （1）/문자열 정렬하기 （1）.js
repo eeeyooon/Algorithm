2675484207d1c9aa64056b16bc6f8b3feb79501e
jiyoon <102462534+eeeyooon@users.noreply.github.com>
2022-12-21 22:51:56 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,10 +1,3 @@
 function solution(my_string) {
-  let answer = [];
-  for (let x of my_string) {
-    if (x.charCodeAt() > 47 && x.charCodeAt() < 58) {
-      answer.push(Number(x))
-    }
-  }
-  answer.sort()
-  return answer;
+    return my_string.replace(/[^\d]/g,'').split('').map(str=>+str).sort();
 }
