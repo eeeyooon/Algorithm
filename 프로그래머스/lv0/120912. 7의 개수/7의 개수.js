b05912f34b4017910624fb406099a2a7de9080b4
jiyoon <102462534+eeeyooon@users.noreply.github.com>
2023-01-04 00:36:50 +0900

    [level 0] Title: 7의 개수, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
@@ -0,0 +1,12 @@
+function solution(array) {
+    
+    return array.join('').split('').filter(a => a === '7').length;
+    
+
+}
+
+
+//천재
+// function solution(array) {
+//     return array.join('').split('7').length - 1;
+// }
