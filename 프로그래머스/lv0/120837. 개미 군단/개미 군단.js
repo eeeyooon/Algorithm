f3b5dad5c028e4d9b9339946f373144542bdf6e4
jiyoon <102462534+eeeyooon@users.noreply.github.com>
2023-01-06 16:36:54 +0900

    [level 0] Title: 개미 군단, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -0,0 +1,9 @@
+function solution(hp) {
+    
+    let lgAnt = Math.floor(hp/5);
+    let mdAnt = Math.floor((hp - lgAnt * 5) / 3); 
+    let smAnt = hp - (lgAnt * 5) - (mdAnt * 3);
+    
+    return lgAnt + mdAnt + smAnt;
+    
+}
