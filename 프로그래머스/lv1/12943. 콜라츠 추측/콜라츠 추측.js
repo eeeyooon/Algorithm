7ccd40cab794a9cde616a9dfb089ed6c32d86dde
eeeyooon <102462534+eeeyooon@users.noreply.github.com>
2023-01-11 17:10:32 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,21 @@
+function solution(num) {
+    
+    for (let i=0; i<500; i++) {
+        
+        if (num !== 1) {
+            
+            if (num % 2 === 0) {
+                num = num/2
+            } else if (num % 2 === 1) {
+                num = num * 3 + 1
+            }   
+            
+        } else {
+            return i;
+        }
+        
+    }
+    
+    return -1;
+    
+}
