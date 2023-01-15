dd0b66ab288d1a4035c0c04667ba1330fc20be60
eeeyooon <102462534+eeeyooon@users.noreply.github.com>
2023-01-15 11:35:03 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,17 @@
+function solution(strings, n) {
+    
+   return strings.sort((a,b) => {
+        
+            if (a[n] > b[n]) return 1;
+
+            else if (a[n] < b[n]) return -1;
+
+            else return a > b ? 1 : -1; 
+        })
+
+
+}
+
+// 이 함수가 리턴하는 값이 0보다 작을 경우,  a가 b보다 앞에 오도록 정렬하고,
+// 이 함수가 리턴하는 값이 0보다 클 경우, b가 a보다 앞에 오도록 정렬합니다.
+// 만약 0을 리턴하면, a와 b의 순서를 변경하지 않습니다.
