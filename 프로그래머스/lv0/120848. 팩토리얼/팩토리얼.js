e8bc9663a26cd93fa273cd76955da60ee15b1f1f
eeeyooon <102462534+eeeyooon@users.noreply.github.com>
2023-01-15 10:53:18 +0900

    [level 0] Title: 팩토리얼, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
@@ -1,17 +1,23 @@
 function solution(n) {
     
-    let num = 1;
+//     let num = 1;
     
-    for (let i=1; i <= n; i++) {
-        num *= i;
+//     for (let i=1; i <= n; i++) {
+//         num *= i;
         
-        if (num === n) {
-            return i;
-        } 
+//         if (num === n) {
+//             return i;
+//         } 
         
-        if (num > n) {
-            return i-1;
-        }
-    }
+//         if (num > n) {
+//             return i-1;
+//         }
+//     }
+    
+        let i = 1;
+        let f = 1;
+    
+        while (f*i < n) f*=++i;
+        return i;
     
 }
