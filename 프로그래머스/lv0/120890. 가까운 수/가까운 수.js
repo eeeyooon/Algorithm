8ca0a8598f0bb8d717d132268654f68579d573c0
eeeyooon <102462534+eeeyooon@users.noreply.github.com>
2023-01-17 20:53:19 +0900

    [level 0] Title: 가까운 수, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -1,13 +1,14 @@
 function solution(array, n) {
     
-    let ans = [];
+//     let ans = [];
+//     for (let num of array) {
+//         ans.push(Math.abs(num - n))
+//     }
+//     const min = Math.min(...ans);
     
-    for (let num of array) {
-        ans.push(Math.abs(num - n))
-    }
-        
-    const min = Math.min(...ans);
-  
+    //위 내용을 한줄로
+    const min = Math.min(...array.map(a => Math.abs(a - n)));
+    
     return array.sort((a,b)=> a-b).find(a => Math.abs(a - n) === min);
     
 }
