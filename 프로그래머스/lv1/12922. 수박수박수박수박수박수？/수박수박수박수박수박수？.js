110af8eceb92e05b23b37520295b2e041194d85c
eeeyooon <102462534+eeeyooon@users.noreply.github.com>
2023-01-21 13:25:55 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,9 +1,7 @@
 function solution(n) {
-    const 수 = '수';
-    const 박 = '박';
     let answer = '';
     for (let i=0; i<n; i++){
-        i % 2 === 0 ? answer += 수 : answer += 박;
+        i % 2 === 0 ? answer += '수' : answer += '박';
     }
     
     return answer
