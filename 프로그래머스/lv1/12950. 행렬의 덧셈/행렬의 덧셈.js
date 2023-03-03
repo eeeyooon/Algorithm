51d0392c1679dadb87b04ce4e7500d6562defade
jiyoon <102462534+eeeyooon@users.noreply.github.com>
2023-03-03 13:45:10 +0900

    [level 1] Title: 행렬의 덧셈, Time: 7.70 ms, Memory: 67.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,11 +1,7 @@
 function solution(arr1, arr2) {
-    let answer = [];
-    for (let i = 0; i < arr1.length; i++) {
-    let ans_row = [];
-    for (let j = 0; j < arr1[0].length; j++) {
-      ans_row.push(arr1[i][j] + arr2[i][j]);
-    }
-    answer.push(ans_row);
-  }
+  const answer = [];
+  arr1.forEach((row, rowIndex) => {
+    answer.push(row.map((col, colIndex) => col + arr2[rowIndex][colIndex]));
+  });
   return answer;
 }
