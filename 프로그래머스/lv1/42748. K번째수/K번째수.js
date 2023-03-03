86b8fef7cafba60403dfe9fb4ca783ffdb8d3afe
jiyoon <102462534+eeeyooon@users.noreply.github.com>
2023-03-03 13:46:06 +0900

    [level 1] Title: K번째수, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -0,0 +1,8 @@
+function solution(array, commands) {
+  var answer = [];
+  commands.forEach(([i, j, k]) => {
+    const su = array.slice(i - 1, j).sort((a, b) => a - b)[k - 1];
+    answer.push(su);
+  });
+  return answer;
+}
