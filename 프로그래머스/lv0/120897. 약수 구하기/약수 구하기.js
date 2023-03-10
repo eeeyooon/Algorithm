128b6207f557ec50dd4ad76559e23a4566c47147
jiyoon <102462534+eeeyooon@users.noreply.github.com>
2023-03-10 20:18:03 +0900

    [level 0] Title: 약수 구하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
@@ -1,11 +1,8 @@
 function solution(n) {
-    
-    let answer = [];
-    for (let i=1; i<=n; i++) {
-        if (n%i === 0) {
-            answer.push(i); 
-        }
-    }
-    
-    return answer;
+  let answer = [];
+  for (let i = 1; i < Math.sqrt(n); i++) {
+    if (n % i === 0) answer = [...answer, i, n / i];
+  }
+  if (Number.isInteger(Math.sqrt(n))) answer = [...answer, Math.sqrt(n)];
+  return answer.sort((a, b) => a - b);
 }
