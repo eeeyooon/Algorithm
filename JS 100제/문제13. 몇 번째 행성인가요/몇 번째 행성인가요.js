0946a54083a2855b0f2306ff21514ed0b868ac1a
jiyoon <vywns9978@gmail.com>
2023-04-03 23:44:56 +0900

    [제코베 JS 100제] 문제11-14번

---
+++ JS 100제/문제13. 몇 번째 행성인가요/몇 번째 행성인가요.js
@@ -0,0 +1,16 @@
+function solution(n) {
+  const planet = [
+    "수성",
+    "금성",
+    "지구",
+    "화성",
+    "목성",
+    "토성",
+    "천왕성",
+    "해왕성",
+  ];
+
+  console.log(planet[n - 1]);
+}
+
+solution(1);
---
+++ JS 100제/문제14. 3의 배수인가요/3의 배수인가요.js
@@ -0,0 +1,9 @@
+function solution(n) {
+  if (n % 3 === 0) {
+    console.log("짝");
+  } else {
+    console.log(n);
+  }
+}
+
+solution(2);
