4d3de8f6da0039cc92e9eb369c646d906b587a69
jiyoon <102462534+eeeyooon@users.noreply.github.com>
2023-05-08 18:09:14 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,6 +1,5 @@
 function solution(my_string) {
-    var answer = '';
     
-    answer = [...my_string].map(a => a.charCodeAt() >= 65 && a.charCodeAt() < 97 ? a.toLowerCase() : a.toUpperCase()).join("");
-    return answer;
+     return answer = [...my_string].map(a => a.charCodeAt() >= 65 && a.charCodeAt() < 97 ? a.toLowerCase() : a.toUpperCase()).join("");
+   
 }
