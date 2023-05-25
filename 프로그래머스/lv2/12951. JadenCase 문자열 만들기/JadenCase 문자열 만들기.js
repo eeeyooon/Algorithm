538e63ca48feddf02162309c851bc1d065fd60c2
jiyoon <vywns9978@gmail.com>
2023-05-25 21:42:11 +0900

    [메모] 빈문자열에 접근하는 charAt()

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,5 +1,14 @@
 function solution(s) {
-    return s.toLowerCase()
+  return s
+    .toLowerCase()
     .split(" ")
-    .map((v) => v.charAt(0).toUpperCase() + v.substring(1)).join(" ");
+    .map((v) => v.charAt(0).toUpperCase() + v.substring(1))
+    .join(" ");
 }
+
+/*
+빈문자열을 접근할때 [0]와 charAt(0)의 차이.
+
+- “”.charAt(0)은 “” 빈문자열을 그대로 돌려줌
+- “”[0]은 undefined를 돌려줌.
+*/
