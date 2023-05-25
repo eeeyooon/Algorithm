4828e31fbb2768a123d6f0aaf4e68bc314a64f88
jiyoon <102462534+eeeyooon@users.noreply.github.com>
2023-05-25 19:13:10 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,16 +1,12 @@
 function solution(n)
 {
-    var answer = 0;
-
-    let num = n.toString();
     
-    for (let a of num) {
+    const newArr = n.toString().split("");
+    let answer = 0;
+    for (let a of newArr) {
         answer += +a;
     }
     
-    
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    console.log('Hello Javascript')
-
     return answer;
+
 }
