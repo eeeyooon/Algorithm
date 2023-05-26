84b53f912e6ff46468da9d212280803f64f88160
jiyoon <102462534+eeeyooon@users.noreply.github.com>
2023-05-27 00:47:03 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,17 +1,18 @@
 function solution(s) {
-    
     let answer = true;
     if (s.length == 4 || s.length == 6) {
-        
+
        for (let num of s) {
-           if (isNaN(Number(num))) answer = false;  
+           if (isNaN(Number(num))) answer = false;
        }
-        
+
     } else {
         answer = false;
     }
-    
+
     return answer;
+
+
 }
 
 //Number는 "e"일때도 숫자로 생각해서 지수로 변환함
