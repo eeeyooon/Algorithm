02f7e3db49514d00ccfbdca2cddf74195d898e93
jiyoon <vywns9978@gmail.com>
2023-05-26 18:50:50 +0900

    [메모] Math.max()와 Math.min()의 매개변수로 배열을 사용할 때

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,6 +1,13 @@
 function solution(arr) {
-    
-    arr.splice(arr.indexOf(Math.min(...arr)), 1);
-    return arr.length ? arr : [-1];
-
+  arr.splice(arr.indexOf(Math.min(...arr)), 1);
+  return arr.length ? arr : [-1];
 }
+
+/*
+메모
+
+Math.min()과 Math.max()는 매개변수 중 하나라도 숫자가 아니거나 숫자로 변환할 수 없다면 NaN을 반환한다. 비슷하게 매개변수로 숫자의 배열인 객체를 전달하면 NaN을 반환한다. 왜냐하면 숫자배열인 객체는 배열 단일 항목으로 취급되고 이 객체를 숫자값으로 변환할 수 없기 때문이다.
+
+그러나 "...연산자"(전개구문 연산자)를 사용하면 배열 객첼르 풀어서 전달하여 올바른 결과를 얻을 수 있다.
+
+*/
