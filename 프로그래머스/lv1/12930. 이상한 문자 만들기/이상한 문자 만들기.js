6c5da5d81afa3466d63c257a4c6a69069287e294
jiyoon <102462534+eeeyooon@users.noreply.github.com>
2023-05-27 19:01:05 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,12 +1,22 @@
 function solution(s) {
-  let answer = '';
-  s.split(" ").forEach((word) => {
-      for(let i = 0, len = word.length; i < len; i++) {
-          if(i % 2 === 0) answer+=word[i].toUpperCase();
-          else answer+=word[i].toLowerCase();
-      }
-      answer += " ";
-  })
-  answer = answer.slice(0, answer.length - 1)
-  return answer;
+    //split()으로 배열로 만들고
+    //짝수일때 대문자 홀수일때 소문자
+    
+    let result = "";
+        s.split(" ").forEach((word) => {
+            for (let i=0; i<word.length; i++) {
+                if (i%2 === 0) {
+                    result += word[i].toUpperCase();
+                    
+                } else {
+                    result += word[i].toLowerCase();
+                    
+                }
+                
+            }
+            
+                result += " ";
+        })
+    
+        return result.substring(0, result.length - 1);
 }
