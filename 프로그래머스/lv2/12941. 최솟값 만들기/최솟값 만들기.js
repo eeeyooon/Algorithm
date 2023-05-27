eb41d317640b5ac1f95a44800fdfb696b1a7c5b1
jiyoon <102462534+eeeyooon@users.noreply.github.com>
2023-05-27 19:40:24 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.79 ms, Memory: 32.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,5 +1,12 @@
 function solution(A, B){
-    A.sort((a, b) => a - b);
-    B.sort((a, b) => b - a);
-    return A.reduce((ac, v, i) => ac + v * B[i], 0);
+    //제일 작은 값이랑 제일 큰값이랑 곱해야 최솟값아님?
+    //그럼 A는 오름차순, B는 내림차순으로 같은 인덱스끼리 곱해서 더하기
+    
+    A.sort((a,b) => a-b);
+    B.sort((a,b) => b-a);
+    
+    return A.reduce((acc, val, idx) => {
+        return acc += (val * B[idx]);
+    }, 0)
+    
 }
