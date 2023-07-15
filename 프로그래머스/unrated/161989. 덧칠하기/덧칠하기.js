f1bc5b86c9ed8607153f3bc3416812e70043747c
jiyoon <102462534+eeeyooon@users.noreply.github.com>
2023-07-15 23:23:50 +0900

    [unrated] Title: 덧칠하기, Time: 1.42 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
@@ -1,11 +1,20 @@
-function solution(n, m, sections) {
-    var answer = 0;
-    var painted = 0;
-    for(var section of sections) {
-        if(painted < section) {
-            answer++;
-            painted = section+m-1;
-        }
+function solution(n, m, section) {
+    let paintCnt = 0; //칠한 횟수
+    let painted = 0; //칠한 구역
+    
+    section.forEach((n) => {
+
+    // 현재 구역이 현재까지 칠한 구역보다 크다면
+    if (n > painted) {
+
+      // 구역을 칠해주고 현재까지 칠한 구역을 업데이트 시켜준다.
+      painted = n + m - 1;
+
+      // 페인트를 칠했으니
+      paintCnt++;
     }
-    return answer;
+        
+  });
+    
+    return paintCnt;
 }
