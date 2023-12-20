6f3947f842f8705959d409629800ad2a73b0e4a8
jiyoon <vywns9978@gmail.com>
2023-12-21 02:04:07 +0900

    [level 1] Title: 푸드 파이트 대회 Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,15 +1,10 @@
 function solution(food) {
-    let arr1 = []
-    for (let i=1; i<food.length; i++) {
-         if (food[i] > 1) {
-             let index = 0;
-             while (index !== ~~(food[i]/2)) {
-                 arr1.push(i);
-                 index++
-             }
-         }
-    }
-    let arr2 = [...arr1].reverse();
-    let answer = arr1.join("")+"0"+arr2.join("")
-    return answer;
+  let temp = "";
+
+  for (let i = 1; i < food.length; i++) {
+    temp += i.toString().repeat(~~(food[i] / 2));
+  }
+
+  let answer = temp + "0" + [...temp].reverse().join("");
+  return answer;
 }
