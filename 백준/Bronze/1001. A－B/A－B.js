282013fb7d4287eaa992c5f23baa6f80b8e5c5ca
jiyoon <102462534+eeeyooon@users.noreply.github.com>
2023-12-20 17:06:25 +0900

    [Bronze V] Title: A-B, Time: 128 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.js
+++ 백준/Bronze/1001. A－B/A－B.js
@@ -1,13 +1,2 @@
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-
-let input = [];
-
-readline.on('line', function(line) {
-    input = line.split(' ').map(el => parseInt(el));
-}).on('close', function(){
-	console.log(input[0]-input[1]);
-    process.exit();
-});
+const input = require('fs').readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim().split(" ").map(Number);
+console.log(input[0]-input[1])
