f636897af572368fa0b9b9496809d410376820b3
jiyoon <102462534+eeeyooon@users.noreply.github.com>
2024-01-08 18:56:09 +0900

    [level 1] Title: 실패율, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,18 +1,20 @@
 function solution(N, stages) {
     let failureRates = [];
+    let totalPlayer = stages.length;
     
-    for (let i=1; i<=N; i++) {
-        let totalPlayer = stages.filter(stage => stage >= i).length;
-        // 도달한 스테이지가 i 이상인 요소들의 개수 == 해당 스테이지에 도달한 플레이어 수
-        let failed = stages.filter(stage => stage == i).length;
-        // 현재 스테이지와 일치하는 요소의 개수 == 해당 스테이지에서 실패한 플레이어 수
-        let failureRate = failed / totalPlayer;
-        failureRates.push({stage:i, rate:failureRate})
-        // 스테이지 번호와 실패율을 함께 추가함
-    }
+  for (let i=1; i<=N; i++)  {
+      let failedPlayer = stages.filter(v=> v=== i).length;
+      let failureRatio = 0;
+      
+      if (failedPlayer === 0) failureRatio = 0;
+      else failureRatio = (failedPlayer) / totalPlayer;
+      
+      totalPlayer -= failedPlayer;
+      
+        failureRates.push({stage:i, rate:(failureRatio)})
+        
+  }  
     
     failureRates.sort((a,b) => b.rate - a.rate);
-    // 내림차순 정렬
     return failureRates.map(v=>v.stage);
-    // 실패율을 기준으로 내림차순 정렬한 전체 실패율 배열에서 스테이지 번호만 리턴.
 }
