d569cb87fbfc4b976029e5ec625e0214ecfa3ece
jiyoon <102462534+eeeyooon@users.noreply.github.com>
2024-01-10 22:18:33 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,53 +1,10 @@
-function solution(lottos, win_nums) {  
-    let MaxRank = 0, MinRank = 0, correctCount = 0, unknownNums = 0;
+function solution(lottos, win_nums) {
+    const answer = [];
+    const min = lottos.filter(n => win_nums.includes(n)).length;
+    const max = lottos.filter(n => n === 0).length + min;
+    
+    max > 1 ? answer.push(7 - max) : answer.push(6);
+    min > 1 ? answer.push(7 - min) : answer.push(6);
 
-    for (let i=0; i<lottos.length; i++) {
-        if (lottos.includes(win_nums[i])) {
-            correctCount++;
-        }
-        if (lottos[i] === 0) unknownNums++;
-    }
-    
-    
-    switch(unknownNums + correctCount) {
-        case 6: 
-            MaxRank = 1;
-            break;
-        case 5: 
-            MaxRank = 2;
-            break;
-        case 4:
-            MaxRank = 3;
-            break;
-        case 3: 
-            MaxRank = 4;
-            break;
-        case 2: 
-            MaxRank = 5;
-            break;
-        default:
-            MaxRank = 6;
-    }
-    
-    switch (correctCount) {
-        case 6:
-            MinRank = 1;
-            break;
-        case 5:
-            MinRank = 2;
-            break;
-        case 4:
-            MinRank = 3;
-            break;
-        case 3:
-            MinRank = 4;
-            break;
-        case 2:
-            MinRank = 5;
-            break;
-        default:
-            MinRank = 6;
-    }
-    
-    return [MaxRank, MinRank];
+    return answer;
 }
