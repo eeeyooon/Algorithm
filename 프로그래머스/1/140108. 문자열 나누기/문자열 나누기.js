03c92ad1d9e2f5b850e570c2b4437b4666425faa
jiyoon <102462534+eeeyooon@users.noreply.github.com>
2024-01-17 15:25:47 +0900

    [level 1] Title: 문자열 나누기, Time: 29.18 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
@@ -0,0 +1,23 @@
+function solution(s) { 
+    let stack = [];
+    let count = 0;
+    
+    for (let i=0; i<s.length; i++) {
+        stack.push(s[i]);
+        const same = stack.filter((str) => str === stack[0] );
+        const notSame = stack.filter((str) => str !== stack[0]);
+        
+        if (same.length === notSame.length) {
+            count += 1;
+            stack = [];
+        }
+    }
+    
+    if (stack.length !== 0) {
+        count += 1;
+    }
+    
+    return count;
+    
+    
+}
